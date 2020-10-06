64c9b04d3fb249186587769b9d25011c5637fd38
Calvin-Davidson <calvin2003davidson@gmail.com>
2020-10-06 14:54:41 +0200

    added header files for School.h and Course.h

---
+++ CPP_Opdracht1/School.cpp
@@ -0,0 +1,26 @@
+#include "School.h"
+
+void School::AddStudent(Course course)
+{
+
+}
+
+void School::RemoveStudent(Course course)
+{
+
+}
+
+void School::NextYear()
+{
+	this->age += 1;
+}
+
+void School::AddCourse(Course course)
+{
+	this->courses.push_back(course);
+}
+
+void School::RemoveCourse(Course course)
+{
+
+}
